bf8990789aa49755400ca303c1a185d5e0962791
Xingjian Shi <xshiab@connect.ust.hk>
2020-02-20 10:56:03 -0800

    Update unix_cmake_command.sh

--- unix_cmake_command.sh
+++ unix_cmake_command.sh
@@ -1,4 +1,11 @@
+cmake -GNinja -D CMAKE_BUILD_TYPE=Release -D BUILD_opencv_gpu=OFF -D CMAKE_INSTALL_PREFIX=/home/ubuntu/.local/ ..
+
+
 rm -rf build
 mkdir -p build && cd build
-cmake -GNinja -C ../config.cmake ..
-cmake --build . --parallel 8
+cmake -GNinja -D OPENCV_ROOT=~/.local/lib/cmake/opencv4/ -C ../config.cmake ..
+cmake --build .
+
+
+
+cmake -GNinja -D OpenCV_DIR=/usr/local/Cellar/opencv@3/3.4.5_2/share/OpenCV -C ../config/darwin.cmake ..
